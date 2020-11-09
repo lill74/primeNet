b9a1674ae4971de1dce34463a9e350bbf126cf90
Yi.Soul <lill74@YiSoului-MacBookPro-3.local>
2020-11-10 03:04:28 +0900

    add adsens and analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
-import App from 'next/app';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
-MyApp.getInitialProps = async (appContext) => ({
-  ...(await App.getInitialProps(appContext)),
-});
+  return <Component {...pageProps} />;
+};
 
-export default MyApp;
+export default App;
